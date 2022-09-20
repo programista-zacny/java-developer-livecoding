bc4c420810aa81edd58285bfcd489dacc1f40d14
Jacek Łacny <programista.zacny@gmail.com>
2022-09-20 11:56:41 +0200

    Init commit

---
+++ src/main/java/pl/programistazacny/javadeveloperlivecoding/JavaDeveloperLiveCodingApplication.java
@@ -0,0 +1,13 @@
+package pl.programistazacny.javadeveloperlivecoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDeveloperLiveCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDeveloperLiveCodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/programistazacny/javadeveloperlivecoding/JavaDeveloperLiveCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.programistazacny.javadeveloperlivecoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDeveloperLiveCodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
